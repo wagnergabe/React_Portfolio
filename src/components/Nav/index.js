90d21971d0af3f7bdedea3f0ae5b89470a0cc3d5
GSvenny <gabrielswagner7@gmail.com>
2022-09-10 20:15:28 -0500

    added hamgurer toggler

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Nav(props) {
+function Nav() {
     return(
         <nav className ='nav'>
             <a href ="#" className = "brand">Gabriel Wagner</a>
@@ -9,8 +9,13 @@ function Nav(props) {
                 <li className="navItem"><a href="#" className="navLink">About</a></li>
                 <li className="navItem"><a href="#" className="navLink">Portfolio</a></li>
                 <li className="navItem"><a href="#" className="navLink">Contact</a></li>
-                <li className="navItem"><a href="#" className="navLink">Contact</a></li>
             </ul>
+            <div className="hamburger">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+                
+            </div>
         </nav>
     )
 }
