d66f4c2f3573fb50629e9f70a65fabcc99e4d375
GSvenny <gabrielswagner7@gmail.com>
2022-09-10 20:36:27 -0500

    toggler framework complete

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Nav() {
+    const [active, setActive] = useState('navMenu')
+        const hamburgerToggle = () => {
+            active === 'navMenu'
+            ? setActive('navMenu navActive')
+            : setActive('navMenu');
+        };
+
+
     return(
         <nav className ='nav'>
-            <a href ="#" className = "brand">Gabriel Wagner</a>
-            <ul className="navMenu">
+            <a href ="#" className = "brand">
+                Gabriel Wagner
+                </a>
+            <ul className={active}>
                 <li className="navItem"><a href="#" className="navLink">Home</a></li>
                 <li className="navItem"><a href="#" className="navLink">About</a></li>
                 <li className="navItem"><a href="#" className="navLink">Portfolio</a></li>
                 <li className="navItem"><a href="#" className="navLink">Contact</a></li>
             </ul>
-            <div className="hamburger">
+            <div onClick ={hamburgerToggle} className="hamburger">
                 <div className="line1"></div>
                 <div className="line2"></div>
                 <div className="line3"></div>
