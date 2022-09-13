67cff35f5e8645bde2d6a3cb24ed30e37bf2411d
GSvenny <gabrielswagner7@gmail.com>
2022-09-12 21:24:34 -0500

    set up pages with potential links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FaGithub } from 'react-icons/fa'
 import { BsLinkedin } from 'react-icons/bs'
+import Nav from '../Nav'
 
 
 const Footer = () => {
+    const [active, setActive] = useState('iconActive')
+    const iconView = () => {
+        active === 'iconActive'
+        ? setActive('footer iconActive')
+        : setActive ('iconActive')
+    }
   return (
     <footer>
-      <div className="footerIcons">
+      <div className={active}>
+        <div className="footerIcons">
         <a href="https://github.com/wagnergabe" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
         <a href="https://www.linkedin.com/" target="blank" rel="noopener noreferrer"><BsLinkedin /></a>
+        </div>
       </div>
 
       {/* <div className="footerCopyright">
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -15,17 +15,17 @@ function Nav() {
             : setToggleIcon('hamburger')
         };
 
-
+        const [activeNav, setActiveNav] = useState('#')
     return(
         <nav className ='nav'>
             <a href ="#" className = "brand">
                 Gabriel Wagner
                 </a>
             <ul className={active}>
-                <li className="navItem"><a href="#" className="navLink">Home</a></li>
-                <li className="navItem"><a href="#" className="navLink">About</a></li>
-                <li className="navItem"><a href="#" className="navLink">Portfolio</a></li>
-                <li className="navItem"><a href="#" className="navLink">Contact</a></li>
+                <li className="navItem"><a href="#" onClick={() => setActiveNav('#home')} className="navLink">Home</a></li>
+                <li className="navItem"><a href="#about" onClick={() => setActiveNav('#about')} className="navLink">About</a></li>
+                <li className="navItem"><a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className="navLink">Portfolio</a></li>
+                <li className="navItem"><a href="#Contact" onClick={() => setActiveNav('#Contact')} className="navLink">Contact</a></li>
             </ul>
             <div onClick ={hamburgerToggle} className={toggleIcon}>
                 <div className="line1"></div>
