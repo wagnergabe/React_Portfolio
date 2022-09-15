824ab4c32a25bc34f6835496c0669b9e8cb60050
GSvenny <gabrielswagner7@gmail.com>
2022-09-15 18:41:59 -0500

    fixed icon overlap

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,9 +3,12 @@ import gabeImage from '../../images/Oyamu.jpg'
 
 const About = () => {
     return (
-        <div>
-        <h1> About </h1>
+        <div className='aboutContainer'>
+        <h1> About Me </h1>
             <img src={gabeImage} alt='Image of Gabriel Wagner'/>
+            <p>Welcome to my First React Project!</p>
+            <p>I am a soon-to-be graduate of the University of Minnesota's full-stack web developer program. </p>
+            <p>Enjoy my site!</p>
         </div>
     )
 }
