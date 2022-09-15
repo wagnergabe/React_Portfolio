8ea9749ebbac8a799a07647dc460942acd14d314
GSvenny <gabrielswagner7@gmail.com>
2022-09-15 13:42:47 -0500

    added z index to react components

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,52 @@
 import React from 'react'
+import {
+    faNodeJs,
+    faCss3Alt,
+    faGithub,
+    faHtml5,
+    faJsSquare,
+    faReact,
+  } from '@fortawesome/free-brands-svg-icons'
+import { useEffect } from 'react'
+import './index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const About = () => {
+
     return (
-        <div>
+        <div className='container about-page'>
+            <div className='text-zone'>
         <h1> About </h1>
+        <p>about me</p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faNodeJs} color="#3c873a" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3Alt} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGithub} color="#211F1F" />
+            </div>
+          </div>
+        </div>
+
         </div>
     )
 }
 
 export default About
+
+//cube animation Source: https://www.youtube.com/watch?v=bmpI252DmiI&t=737s
