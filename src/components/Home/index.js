fdd5b7019c975b05c7b19853a594087cd18c0c89
GSvenny <gabrielswagner7@gmail.com>
2022-09-16 01:21:32 -0500

    added intro animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,24 @@
-import React from "react";
-
+import React, {useEffect, useRef} from "react";
+import Lottie from "lottie-web";
 const Home = () => {
-    return (
-        <div>
-            <p>Home</p>
-        </div>
-    )
-}
+
+    
+    const container = useRef(null)
+            useEffect(() => {
+
+
+                Lottie.loadAnimation({
+                    container: container.current,
+                    renderer: 'svg',
+                    loop: true,
+                    autoplay: true,
+                    animationData: require('../../animate.json')
+                })
+            }, [])
+
+        return(
+            <div className="container" ref={container}></div>
+        )
+        }
 
 export default Home;
